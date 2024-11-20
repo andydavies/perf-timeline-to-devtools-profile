0b66b20c9894f90fb4dac3ac18943f9f18a16638
Andy Davies <ad@andydavies.net>
2024-11-20 16:04:26 +0000

    Add more detail to LoAF and script timing events. Normalise quotes

--- content-script.js
+++ content-script.js
@@ -12,15 +12,15 @@ obs = new PerformanceObserver((list) => {
         if(entry.entryType == 'event' && entry.interactionId > 0) {
             inpEventCount++;
 
-            performance.measure("Input Delay", {
+            performance.measure('Input Delay', {
                 start: entry.startTime,
                 end: entry.processingStart, 
                 detail: {
                     devtools: {
-                        dataType: "track-entry",
-                        track: "INP." + inpEventCount,
-                        trackGroup: "INP",
-                        color: "primary",
+                        dataType: 'track-entry',
+                        track: 'INP.' + inpEventCount,
+                        trackGroup: 'INP',
+                        color: 'primary-light',
                         tooltipText: 'Input Delay',
                         properties: [
                             ['name', '' + entry.name],
@@ -29,15 +29,15 @@ obs = new PerformanceObserver((list) => {
                     }
                 }
             });
-            performance.measure("Processing Time", {
+            performance.measure('Processing Time', {
                 start: entry.processingStart,
                 end: entry.processingEnd, 
                 detail: {
                     devtools: {
-                        dataType: "track-entry",
-                        track: "INP." + inpEventCount,
-                        trackGroup: "INP",
-                        color: "primary",
+                        dataType: 'track-entry',
+                        track: 'INP.' + inpEventCount,
+                        trackGroup: 'INP',
+                        color: 'primary',
                         tooltipText: 'Processing Time',
                         properties: [
                             ['name', '' + entry.name],
@@ -46,17 +46,18 @@ obs = new PerformanceObserver((list) => {
                     }
                 }
             });
-            performance.measure("Presentation Delay", {
+            performance.measure('Presentation Delay', {
                 start: entry.processingEnd,
                 end: entry.startTime + entry.duration, 
                 detail: {
                     devtools: {
-                        dataType: "track-entry",
-                        track: "INP." + inpEventCount,
-                        trackGroup: "INP",
-                        color: "primary",
+                        dataType: 'track-entry',
+                        track: 'INP.' + inpEventCount,
+                        trackGroup: 'INP',
+                        color: 'primary-light',
                         tooltipText: 'Presentation Delay',
                         properties: [
+                            ['interactionId', '' + entry.interactionId],
                             ['name', '' + entry.name],
                             ['target', '' + entry.target]
                         ]
@@ -68,47 +69,73 @@ obs = new PerformanceObserver((list) => {
 
             // Set script entries before LoAF so Chrome can display them in the right order
             for(const script of entry.scripts) {
-                performance.measure("script", {
+                performance.measure('script', {
                     start: script.startTime,
                     end: script.startTime + script.duration, 
                     detail: {
                         devtools: {
-                        dataType: "track-entry",
-                        track: "LoAF",
-                        trackGroup: "Performance Timeline",
-                        color: "secondary",
-                        tooltipText: script.sourceURL
+                            dataType: 'track-entry',
+                            track: 'LoAF',
+                            trackGroup: 'Performance Timeline',
+                            color: 'secondary',
+                            tooltipText: script.sourceURL,
+                            properties: [
+                                ['name', '' + script.name],
+                                ['entryType', '' + script.entryType],
+                                ['startTime', '' + script.startTime],
+                                ['duration', '' + script.duration],
+                                ['navigationId', '' + script.navigationId],
+                                ['invoker', '' + script.invoker],
+                                ['invokerType', '' + script.invokerType],
+                                ['windowAttribution', '' + script.windowAttribution],
+                                ['executionStart', '' + script.executionStart],
+                                ['forcedStyleAndLayoutDuration', '' + script.forcedStyleAndLayoutDuration],
+                                ['pauseDuration', '' + script.pauseDuration],
+                                ['sourceURL', '' + script.sourceURL],
+                                ['sourceFunctionName', '' + script.sourceFunctionName],
+                                ['sourceCharPosition', '' + script.sourceCharPosition]
+                            ]
                         }
                     }
                 });
             }
 
-            performance.measure("LoAF", {
+            performance.measure('LoAF', {
               start: entry.startTime,
               end: entry.startTime + entry.duration, 
               detail: {
                 devtools: {
-                  dataType: "track-entry",
-                  track: "LoAF",
-                  trackGroup: "Performance Timeline",
-                  color: "primary",
-                  tooltipText: 'LoAF'
+                  dataType: 'track-entry',
+                  track: 'LoAF',
+                  trackGroup: 'Performance Timeline',
+                  color: 'primary',
+                  tooltipText: 'LoAF',
+                    properties: [
+                        ['name', '' + entry.name],
+                        ['entryType', '' + entry.entryType],
+                        ['startTime', '' + entry.startTime],
+                        ['duration', '' + entry.duration],
+                        ['renderStart', '' + entry.renderStart],
+                        ['styleAndLayoutStart', '' + entry.styleAndLayoutStart],
+                        ['firstUIEventTimestamp', '' + entry.firstUIEventTimestamp],
+                        ['blockingDuration', '' + entry.blockingDuration]
+                    ]
                 }
               }
             });
 
-            if(entry.styleAndLayoutStart > 0) {
-                performance.measure("Layout + Paint", {
-                    start: entry.styleAndLayoutStart,
+            if(entry.renderStart > 0) {
+                performance.measure('Rendering', {
+                    start: entry.renderStart,
                     end: entry.startTime + entry.duration, 
                     detail: {
-                    devtools: {
-                        dataType: "track-entry",
-                        track: "LoAF",
-                        trackGroup: "Performance Timeline",
-                        color: "secondary-light",
-                        tooltipText: 'Layout + Paint'
-                    }
+                        devtools: {
+                            dataType: 'track-entry',
+                            track: 'LoAF',
+                            trackGroup: 'Performance Timeline',
+                            color: 'secondary-light',
+                            tooltipText: 'Rendering'
+                        }
                     }
                 });
             }
@@ -116,15 +143,15 @@ obs = new PerformanceObserver((list) => {
         }
         else if (entry.entryType == 'longtask') {
         
-            performance.measure("Long Task", {
+            performance.measure('Long Task', {
                 start: entry.startTime,
                 end: entry.startTime + entry.duration, 
                 detail: {
                     devtools: {
-                    dataType: "track-entry",
-                    track: "Long Task",
-                    trackGroup: "Performance Timeline",
-                    color: "primary",
+                    dataType: 'track-entry',
+                    track: 'Long Task',
+                    trackGroup: 'Performance Timeline',
+                    color: 'primary',
                     tooltipText: 'Long Task'
                     }
                 }
@@ -136,10 +163,10 @@ obs = new PerformanceObserver((list) => {
                 start: entry.startTime,
                 detail: {
                     devtools: {
-                    dataType: "track-entry",
-                    track: "LCP",
-                    trackGroup: "Performance Timeline",
-                    color: "primary",
+                    dataType: 'track-entry',
+                    track: 'LCP',
+                    trackGroup: 'Performance Timeline',
+                    color: 'primary',
                     tooltipText: 'LCP Candidate',
                     properties: [
                         ['size', '' + entry.size],
